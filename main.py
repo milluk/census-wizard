f20a26427522fb37457d4d9bacdade5dc56cb8a6
Elliot Foster <efoster@efoster-desktop.(none)>
2010-04-07 21:13:19 -0700

    Commit of Troy's changes to main.py and wizard.html

--- main.py
+++ main.py
@@ -16,17 +16,29 @@ class Entry(db.Model):
   date              = db.DateTimeProperty(auto_now=True)
 
   unique_key        = db.StringProperty()
+  first_name        = db.StringProperty()
+  last_name         = db.StringProperty()
   email             = db.StringProperty()
   phone             = db.StringProperty()
   address           = db.StringProperty(multiline=True)
+  streetaddress     = db.StringProperty()
+  streetaddress2    = db.StringProperty()
+  city              = db.StringProperty()
+  state             = db.StringProperty()
+  zip               = db.StringProperty()
+  ownership         = db.StringProperty()
   num_residents     = db.StringProperty()
 
   # wow, it would be really nice if I knew how to dynamically define a class in python
+
   p00_last_name         = db.StringProperty()
   p00_first_name        = db.StringProperty()
   p00_middle_initial    = db.StringProperty()
   p00_sex               = db.StringProperty()
   p00_birth_date        = db.StringProperty()
+  p00_birth_month       = db.StringProperty()
+  p00_birth_day         = db.StringProperty()
+  p00_birth_year        = db.StringProperty()
   p00_hispanic          = db.StringProperty()
   p00_race              = db.StringProperty()
   p00_usual_residence   = db.StringProperty()
@@ -37,6 +49,9 @@ class Entry(db.Model):
   p01_middle_initial    = db.StringProperty()
   p01_sex               = db.StringProperty()
   p01_birth_date        = db.StringProperty()
+  p01_birth_month       = db.StringProperty()
+  p01_birth_day         = db.StringProperty()
+  p01_birth_year        = db.StringProperty()
   p01_hispanic          = db.StringProperty()
   p01_race              = db.StringProperty()
   p01_usual_residence   = db.StringProperty()
@@ -47,6 +62,9 @@ class Entry(db.Model):
   p02_middle_initial    = db.StringProperty()
   p02_sex               = db.StringProperty()
   p02_birth_date        = db.StringProperty()
+  p02_birth_month       = db.StringProperty()
+  p02_birth_day         = db.StringProperty()
+  p02_birth_year        = db.StringProperty()
   p02_hispanic          = db.StringProperty()
   p02_race              = db.StringProperty()
   p02_usual_residence   = db.StringProperty()
@@ -57,6 +75,9 @@ class Entry(db.Model):
   p03_middle_initial    = db.StringProperty()
   p03_sex               = db.StringProperty()
   p03_birth_date        = db.StringProperty()
+  p03_birth_month       = db.StringProperty()
+  p03_birth_day         = db.StringProperty()
+  p03_birth_year        = db.StringProperty()
   p03_hispanic          = db.StringProperty()
   p03_race              = db.StringProperty()
   p03_usual_residence   = db.StringProperty()
@@ -67,6 +88,9 @@ class Entry(db.Model):
   p04_middle_initial    = db.StringProperty()
   p04_sex               = db.StringProperty()
   p04_birth_date        = db.StringProperty()
+  p04_birth_month       = db.StringProperty()
+  p04_birth_day         = db.StringProperty()
+  p04_birth_year        = db.StringProperty()
   p04_hispanic          = db.StringProperty()
   p04_race              = db.StringProperty()
   p04_usual_residence   = db.StringProperty()
@@ -77,6 +101,9 @@ class Entry(db.Model):
   p05_middle_initial    = db.StringProperty()
   p05_sex               = db.StringProperty()
   p05_birth_date        = db.StringProperty()
+  p05_birth_month       = db.StringProperty()
+  p05_birth_day         = db.StringProperty()
+  p05_birth_year        = db.StringProperty()
   p05_hispanic          = db.StringProperty()
   p05_race              = db.StringProperty()
   p05_usual_residence   = db.StringProperty()
@@ -87,6 +114,9 @@ class Entry(db.Model):
   p06_middle_initial    = db.StringProperty()
   p06_sex               = db.StringProperty()
   p06_birth_date        = db.StringProperty()
+  p06_birth_month       = db.StringProperty()
+  p06_birth_day         = db.StringProperty()
+  p06_birth_year        = db.StringProperty()
   p06_hispanic          = db.StringProperty()
   p06_race              = db.StringProperty()
   p06_usual_residence   = db.StringProperty()
@@ -97,6 +127,9 @@ class Entry(db.Model):
   p07_middle_initial    = db.StringProperty()
   p07_sex               = db.StringProperty()
   p07_birth_date        = db.StringProperty()
+  p07_birth_month       = db.StringProperty()
+  p07_birth_day         = db.StringProperty()
+  p07_birth_year        = db.StringProperty()
   p07_hispanic          = db.StringProperty()
   p07_race              = db.StringProperty()
   p07_usual_residence   = db.StringProperty()
@@ -107,6 +140,9 @@ class Entry(db.Model):
   p08_middle_initial    = db.StringProperty()
   p08_sex               = db.StringProperty()
   p08_birth_date        = db.StringProperty()
+  p08_birth_month       = db.StringProperty()
+  p08_birth_day         = db.StringProperty()
+  p08_birth_year        = db.StringProperty()
   p08_hispanic          = db.StringProperty()
   p08_race              = db.StringProperty()
   p08_usual_residence   = db.StringProperty()
@@ -117,6 +153,9 @@ class Entry(db.Model):
   p09_middle_initial    = db.StringProperty()
   p09_sex               = db.StringProperty()
   p09_birth_date        = db.StringProperty()
+  p09_birth_month       = db.StringProperty()
+  p09_birth_day         = db.StringProperty()
+  p09_birth_year        = db.StringProperty()
   p09_hispanic          = db.StringProperty()
   p09_race              = db.StringProperty()
   p09_usual_residence   = db.StringProperty()
@@ -127,6 +166,9 @@ class Entry(db.Model):
   p10_middle_initial    = db.StringProperty()
   p10_sex               = db.StringProperty()
   p10_birth_date        = db.StringProperty()
+  p10_birth_month       = db.StringProperty()
+  p10_birth_day         = db.StringProperty()
+  p10_birth_year        = db.StringProperty()
   p10_hispanic          = db.StringProperty()
   p10_race              = db.StringProperty()
   p10_usual_residence   = db.StringProperty()
@@ -137,6 +179,9 @@ class Entry(db.Model):
   p11_middle_initial    = db.StringProperty()
   p11_sex               = db.StringProperty()
   p11_birth_date        = db.StringProperty()
+  p11_birth_month       = db.StringProperty()
+  p11_birth_day         = db.StringProperty()
+  p11_birth_year        = db.StringProperty()
   p11_hispanic          = db.StringProperty()
   p11_race              = db.StringProperty()
   p11_usual_residence   = db.StringProperty()
@@ -169,6 +214,12 @@ class APIHandler(webapp.RequestHandler):
         'phone',
         'address',
         'address_ownership',
+        'streetaddress',
+        'streetaddress2',
+        'city',
+        'state',
+        'zip',
+        'ownership',
         'num_residents',
         'unique_key',
     ]
@@ -179,6 +230,9 @@ class APIHandler(webapp.RequestHandler):
         'middle_initial',
         'sex',
         'birth_date',
+        'birth_month',
+        'birth_day',
+        'birth_year',
         'hispanic',
         'race',
         'usual_residence',
